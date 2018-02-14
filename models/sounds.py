0ce51432f79030ccb5e6c89b19b9204ed4af4637
Jeff G <jg@okayjeff.com>
2018-02-13 21:27:49 -0500

    Stop all sounds before playing new ones.

--- models/sounds.py
+++ models/sounds.py
@@ -14,35 +14,44 @@ class SoundController:
     THUD = 'thud.wav'
 
     @staticmethod
+    def _stop_all_sound():
+        pygame.mixer.stop()
+
+    @staticmethod
     def _load_sound_file(fname):
         return pygame.mixer.Sound(fname)
 
     @classmethod
     def play_intro(cls):
+        cls._stop_all_sound()
         fname = os.path.join(BASE_PATH, cls.GAME_INTRO)
         sound = cls._load_sound_file(fname)
         sound.play()
 
     @classmethod
     def play_racquet(cls):
+        cls._stop_all_sound()
         fname = os.path.join(BASE_PATH, cls.RACQUET)
         sound = cls._load_sound_file(fname)
         sound.play()
 
     @classmethod
     def play_game_over(cls):
+        cls._stop_all_sound()
         fname = os.path.join(BASE_PATH, cls.GAME_OVER)
         sound = cls._load_sound_file(fname)
         sound.play()
 
     @classmethod
     def play_new_game(cls):
+        cls._stop_all_sound()
         fname = os.path.join(BASE_PATH, cls.START_GAME)
         sound = cls._load_sound_file(fname)
         sound.play()
 
     @classmethod
     def play_thud(cls):
+        cls._stop_all_sound()
         fname = os.path.join(BASE_PATH, cls.THUD)
         sound = cls._load_sound_file(fname)
         sound.play()
